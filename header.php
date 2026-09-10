ba03692e62aa123d55559ad6d2cbcf1de20a9cc9
Sreelakshmi Venoj <venojsreelakshmi76@gmail.com>
2026-09-10 13:30:41 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-
 <html>
 <head>
-	<title>Admin Dashboard</title>
-	<link rel="stylesheet" href="style.css">
+    <title>Little Shots</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-<header>
-	<div class="logo">
-		<h1>Little Shots</h1>
-		<h2>Baby Vaccination Management</h2>
-	</div>
 
-	<nav>
-		<a href="admin_home.php">Home</a>
-		<a href="admin_dashboard.php">Dashboard</a>
-		<a href="manage_vaccines.php">Vaccines</a>
-		<a href="../index.php">Logout</a>
-	</nav>
+<body>
+
+<header>
+    <div class="logo">
+        <h1>Little Shots</h1> 
+         <h2>Baby Vaccination Management</h2>
+    </div>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="login.php">Login</a>
+		<a href="admin/admin_login.php">Admin</a>
+    </nav>
 </header>
